2b9cdde53e1de19459dcc1bbe32237ec72d37a4f
Elena <helenmai@yandex.ru>
2022-09-06 12:55:09 +0300

    voidChoiceNoMistakes

--- Desktop/Geek/Kontrol/KontrolBlok1/Program.cs
+++ Desktop/Geek/Kontrol/KontrolBlok1/Program.cs
@@ -3,37 +3,19 @@
 // с клавиатуры, либо задать на старте выполнения алгоритма. 
 
 
-void Choice(string[] args)
-{
-  string[] Crow = {"Кар", "! ", "ворона ", "прилетела ", "на ", "дубовый ", "сук ",
+ string[] arrayPoetry = {"Кар", "! ", "ворона ", "прилетела ", "на ", "дубовый ", "сук ",
   "Кар", "! ", "другая ", "рядом ", "села ", "в ", "бок ", "соседку ", "тюк "};
-   string[] array2 = new string[Crow.Length];
-   for (int i = 0; i < Crow.Length; i++)
-      Console.Write("{1}", i, Crow[i]);
-}
-Choice(args);
-/*string[] array1 = {"Кар", "!", "ворона", "прилетела", "на", "дубовый", "сук", "Кар", "!", "другая", "рядом", "села", "в", "бок", "соседку", "тюк"};
- 
-  void Choice(string[] array1, string[] array2)
+   string[] arrayWords = new string[arrayPoetry.Length];
+  void Choice(string[] arrayPoetry, string[] arrayWords)
   {
       int count = 0;
-      for (int i = 0; i < array1.Length; i++)
+      for (int i = 0; i < arrayPoetry.Length; i++)
       {
-      if(array1[i].Length <= 3)
+      if(arrayPoetry[i].Length <= 3)
           {
-          array2[count] = array1[i];
+          arrayWords[count] = arrayPoetry[i];
           count++;
           }
       }
   }
-  void PrintArray(string[] array)
-  {
-      for (int i = 0; i < array.Length; i++)
-      {
-          Console.Write($"{array[i]} ");
-      }
-      Console.WriteLine();
-  }
-  Choice(array1, array2);
-PrintArray(array1);
-// PrintArray(array2);*/
+  Choice(arrayPoetry, arrayWords);
