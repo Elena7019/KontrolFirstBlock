b34c951e8c4389ad0d3b4698adbbbe6f01452fdc
Elena <helenmai@yandex.ru>
2022-09-07 12:46:11 +0300

    Solution

--- Desktop/Geek/Kontrol/KontrolBlok1/Program.cs
+++ Desktop/Geek/Kontrol/KontrolBlok1/Program.cs
@@ -2,20 +2,34 @@
 // длинна которых меньше или равна 3 символам. Первоначальный массив можно ввести 
 // с клавиатуры, либо задать на старте выполнения алгоритма. 
 
-
- string[] arrayPoetry = {"Кар", "! ", "ворона ", "прилетела ", "на ", "дубовый ", "сук ",
-  "Кар", "! ", "другая ", "рядом ", "села ", "в ", "бок ", "соседку ", "тюк "};
-   string[] arrayWords = new string[arrayPoetry.Length];
-  void Choice(string[] arrayPoetry, string[] arrayWords)
-  {
-      int count = 0;
-      for (int i = 0; i < arrayPoetry.Length; i++)
-      {
-      if(arrayPoetry[i].Length <= 3)
-          {
-          arrayWords[count] = arrayPoetry[i];
-          count++;
-          }
-      }
+string[] arrayPoetry = {"Кар", "! ", "ворона ", "прилетела ", "на ", "дубовый ", "сук ",
+"\r\n", "Кар", "! ", "другая ", "рядом ", "села ", "в ", "бок ", "соседку ", "тюк "};
+for (int i = 0; i < arrayPoetry.Length; i++)
+    {
+        Console.Write("{1}", i, arrayPoetry[i]);
+    }
+Console.WriteLine();
+string[] arrayWords = new string[arrayPoetry.Length];
+void Choice(string[] arrayPoetry, string[] arrayWords)
+{
+    int count = 0;
+    for (int i = 0; i < arrayPoetry.Length; i++)
+    {
+        if(arrayPoetry[i].Length <= 4)
+        {
+            arrayWords[count] = arrayPoetry[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine("{1}", i, arrayWords[i]);
+    }
+      Console.WriteLine();
   }
-  Choice(arrayPoetry, arrayWords);
+Choice(arrayPoetry, arrayWords);
+
+PrintArray(arrayWords);
