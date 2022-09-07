7167ff706a3e9ddd17420d84086e6322c1df694b
Elena <helenmai@yandex.ru>
2022-09-07 15:46:33 +0300

    Beauty

--- Desktop/Geek/Kontrol/KontrolBlok1/Program.cs
+++ Desktop/Geek/Kontrol/KontrolBlok1/Program.cs
@@ -1,21 +1,24 @@
 ﻿// Напишите программу, которая из имеющегося массива строк формирует массив из строк, 
-// длинна которых меньше или равна 3 символам. Первоначальный массив можно ввести 
+// длина которых меньше или равна 3 символам. Первоначальный массив можно ввести 
 // с клавиатуры, либо задать на старте выполнения алгоритма. 
 
-string[] arrayPoetry = {"Кар", "! ", "ворона ", "прилетела ", "на ", "дубовый ", "сук ",
-"\r\n", "Кар", "! ", "другая ", "рядом ", "села ", "в ", "бок ", "соседку ", "тюк "};
+string[] arrayPoetry = {"Кар", "!", " ворона ","прилетела ", "на", " дубовый ", "сук",
+ "\r\n", "Кар", "!", " другая ", "рядом ","села ", "в ", "бок", " соседку ", "тюк"};
+Console.WriteLine("Введен массив: " );
 for (int i = 0; i < arrayPoetry.Length; i++)
     {
-        Console.Write("{1}", i, arrayPoetry[i]);
+         Console.Write("{1}", i, arrayPoetry[i]);
     }
 Console.WriteLine();
+Console.WriteLine("Преобразовываем в массив из строк, длина которых меньше или равна 3 символам:");
+ 
 string[] arrayWords = new string[arrayPoetry.Length];
 void Choice(string[] arrayPoetry, string[] arrayWords)
 {
-    int count = 0;
+    int count = 0; int x =3;
     for (int i = 0; i < arrayPoetry.Length; i++)
     {
-        if(arrayPoetry[i].Length <= 4)
+        if(arrayPoetry[i].Length <= x)
         {
             arrayWords[count] = arrayPoetry[i];
             count++;
@@ -28,8 +31,7 @@ void PrintArray(string[] array)
     {
         Console.WriteLine("{1}", i, arrayWords[i]);
     }
-      Console.WriteLine();
+    Console.WriteLine();
   }
 Choice(arrayPoetry, arrayWords);
-
 PrintArray(arrayWords);
